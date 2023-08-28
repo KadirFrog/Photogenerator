397ec2667d6148fc78e718cdb5b302085552b915
Kadir Aras Yanar <yanarkadiraras@gmail.com>
2023-08-28 19:21:32 +0300

    Position Update

--- func.py
+++ func.py
@@ -3,7 +3,7 @@ import random
 
 def rotate(SS, angle):
     ssIMG = SS.copy()  # Make a copy of the image before rotating
-    ssIMG = ssIMG.rotate(angle, expand=True, fillcolor=(100, 0, 0, 0))
+    ssIMG = ssIMG.rotate(angle, expand=True, fillcolor=False)
     return ssIMG
 
 def background_p_SS(background, SS, resultname):
@@ -11,8 +11,10 @@ def background_p_SS(background, SS, resultname):
     backgroundIMG = backgroundIMG.resize((2000, 1500), Image.BICUBIC)
     ssIMG = SS.copy()
     ssIMG = ssIMG.resize((700, 700), Image.BICUBIC)
-    x_position = (backgroundIMG.width - ssIMG.width) // 2
-    y_position = (backgroundIMG.height - ssIMG.height) // 2
+    a = random.randint(0, 500)
+    b = random.randint(0, 250)
+    x_position = ((backgroundIMG.width - ssIMG.width) // 2) + a 
+    y_position = ((backgroundIMG.height - ssIMG.height) // 2) + b
     position = (x_position, y_position)
     backgroundIMG.paste(ssIMG, position)  # Remove ssIMG from paste() arguments
     backgroundIMG.save(resultname)
